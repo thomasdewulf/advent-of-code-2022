25becb717c3604e0f58d3026a3957196aa7f0fcd
Thomas de Wulf <thomas.dewulf@upcore.be>
2022-12-02 11:58:37 +0100

    Day 1

---
+++ AdventOfCode2022.Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var calories = File.ReadAllText("input.txt");
+
+var max = calories
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(x => 
+        x.Split(Environment.NewLine).Select(int.Parse)
+            .Sum())
+    .Max();
+
+
+    Console.WriteLine("Elf MAX");
+    Console.WriteLine(max);
+
+    
+
