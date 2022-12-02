a96f810cde0f2ed837e6d9745f8e43f95c1ee8c6
Thomas de Wulf <thomas.dewulf@upcore.be>
2022-12-02 14:17:45 +0100

    Day 2

---
+++ Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿var calories = File.ReadAllText("input.txt");
+
+var max = calories
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(x =>
+        x.Split(Environment.NewLine).Select(int.Parse)
+            .Sum())
+    .OrderByDescending(x => x)
+    .Take(3)
+    .Sum();
+
+
+Console.WriteLine("Elf MAX");
+Console.WriteLine(max);
