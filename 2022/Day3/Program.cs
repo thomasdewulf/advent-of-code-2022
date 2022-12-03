8b5b6160f9b15cea70c2f0eb1807e52a4c06a787
Thomas de Wulf <thomas.de.wulf@telenet.be>
2022-12-03 07:44:39 +0100

    Formatting

--- 2022/Day3/Program.cs
+++ 2022/Day3/Program.cs
@@ -3,8 +3,8 @@
 var input = await File.ReadAllLinesAsync("input.txt");
 
 // PART 1
-var prios = input.
-Select(x => new Rucksack(x)
+var prios = input
+    .Select(x => new Rucksack(x)
         .GetPriority())
     .Sum();
 
@@ -13,12 +13,11 @@ Console.WriteLine(prios);
 // PART 2
 var result = input.Chunk(3) //Group rucksacks per 3 elves
     .Select(rucksacks => rucksacks
-    .Aggregate<IEnumerable<char>>(
-        (previousList, nextList) => 
-        previousList.Intersect(nextList)) //Intersect the 3 rucksack values to get the common value
-    .Single()) //There should only be one common value
+        .Aggregate<IEnumerable<char>>(
+            (previousList, nextList) =>
+                previousList.Intersect(nextList)) //Intersect the 3 rucksack values to get the common value
+        .Single()) //There should only be one common value
     .Select(x => x.GetPriority()) // Get the prio of that value
     .Sum(); // Add upp all the prios to get final prio
 
 Console.WriteLine(result);
-
