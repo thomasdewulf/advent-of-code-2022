acdfdb8c31db217e73353bd053f88499b36c5fdc
Thomas de Wulf <thomas.de.wulf@telenet.be>
2022-12-03 07:38:51 +0100

    Formatting

--- 2022/Day3/Program.cs
+++ 2022/Day3/Program.cs
@@ -13,9 +13,9 @@ Console.WriteLine(prios);
 // PART 2
 var result = input.Chunk(3) //Group rucksacks per 3 elves
     .Select(rucksacks => rucksacks
-    .Aggregate<IEnumerable<char>>((previousList, nextList) => 
-        previousList.
-            Intersect(nextList)) //Intersect the 3 rucksack values to get the common value
+    .Aggregate<IEnumerable<char>>(
+        (previousList, nextList) => 
+        previousList.Intersect(nextList)) //Intersect the 3 rucksack values to get the common value
     .Single()) //There should only be one common value
     .Select(x => x.GetPriority()) // Get the prio of that value
     .Sum(); // Add upp all the prios to get final prio
